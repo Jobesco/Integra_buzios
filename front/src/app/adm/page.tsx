9f7eb8e057433af382175df39b0bd2bc4860abd4
Job <ljba@cesar.org.br>
2025-02-26 22:19:47 -0300

    fixing changes

--- front/src/app/adm/page.tsx
+++ front/src/app/adm/page.tsx
@@ -1,10 +1,21 @@
-
+'use client'
 import { Button } from "@/components/ui/button"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import Image from "next/image"
+import grafico from "@/public/adm/graficop.svg"
+import escudo from "@/public/adm/escudo.svg"
+import faq from "@/public/adm/faq.svg"
+import pasta from "@/public/adm/pasta.svg"
+import pessoa from "@/public/adm/pessoa.svg"
+import prancheta from "@/public/adm/prancheta.svg"
+import {useRouter} from "next/navigation"
 
 
 export default function Dashboard() {
+  const {push} = useRouter()
+
+  // TODO histórico!!!!! só não tem aqui!!
+
   return (
     <div className="container bg-background h-full mt-32
       flex flex-col items-start justify-start gap-8 text-4xl">
@@ -17,7 +28,8 @@ export default function Dashboard() {
         <div className="flex justify-between items-center 
           w-full p-4 bg-gray-200">
           <h2>Meus eventos</h2>
-          <Button className="bg-primary900 text-background px-14 py-2 rounded-full">
+          <Button className="bg-primary900 text-background px-14 py-2 
+            rounded-full" onClick={() => push('/adm/event/new')}>
             Criar novo evento
           </Button>
         </div>
@@ -47,7 +59,7 @@ export default function Dashboard() {
               items-center px-2 py-3 bg-surface rounded-xl text-sm hover:cursor-pointer">
               <span className="px-3 flex gap-1
                 text-primary">
-                <Image width="10" height="10" src="/manage.png" alt="Gerenciar atividades" />
+                <Image unoptimized width="20" height="20" src={prancheta} alt="Gerenciar atividades" />
                 <span className="text-onSurface">Gerenciar atividades</span>
               </span>
               
@@ -59,7 +71,7 @@ export default function Dashboard() {
               items-center px-2 py-3 bg-surface rounded-xl text-sm hover:cursor-pointer">
               <span className="px-3 flex gap-1
                 text-primary">
-                <span className="text-onSurface">IC</span>
+                <Image unoptimized width="20" height="20" src={pasta} alt="Gerenciar atividades" />
                 <span className="text-onSurface">Editar gerências</span>
               </span>
             </div>
@@ -70,7 +82,7 @@ export default function Dashboard() {
               items-center px-2 py-3 bg-surface rounded-xl text-sm hover:cursor-pointer">
               <span className="px-3 flex gap-1
                 text-primary">
-                <span className="text-onSurface">IC</span>
+                <Image unoptimized width="20" height="20" src={pessoa} alt="Gerenciar atividades" />
                 <span className="text-onSurface">Gerenciar contas</span>
               </span>
             </div>
@@ -81,7 +93,7 @@ export default function Dashboard() {
               items-center px-2 py-3 bg-surface rounded-xl text-sm hover:cursor-pointer">
               <span className="px-3 flex gap-1
                 text-primary">
-                <span className="text-onSurface">IC</span>
+                <Image unoptimized width="20" height="20" src={grafico} alt="Gerenciar atividades" />
                 <span className="text-onSurface">Dados do projeto</span>
               </span>
             </div>
@@ -92,7 +104,7 @@ export default function Dashboard() {
               items-center px-2 py-3 bg-surface rounded-xl text-sm hover:cursor-pointer">
               <span className="px-3 flex gap-1
                 text-primary">
-                <span className="text-onSurface">IC</span>
+                <Image unoptimized width="20" height="20" src={escudo} alt="Gerenciar atividades" />
                 <span className="text-onSurface">Gerenciar administradores</span>
               </span>
             </div>
@@ -103,7 +115,7 @@ export default function Dashboard() {
               items-center px-2 py-3 bg-surface rounded-xl text-sm hover:cursor-pointer">
               <span className="px-3 flex gap-1
                 text-primary">
-                <span className="text-onSurface">IC</span>
+                <Image unoptimized width="20" height="20" src={faq} alt="Gerenciar atividades" />
                 <span className="text-onSurface">Editar FAQ</span>
               </span>
             </div>
