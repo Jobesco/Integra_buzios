548f0a169a307386726efa6977a21a9a3cd58725
Miranda <lmirandadasilva@apple.com>
2025-03-13 02:42:54 -0300

    add botoes de login e registro

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -22,7 +22,6 @@ import {
 import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar";
 
 import { usePathname, useRouter } from "next/navigation";
-// import { updateLID, lattesSync } from "@/app_old/actions";
 import { getLocalStorage, delLocalStorage, setLocalStorage } from "@/lib/localStorage";
 import { MouseEventHandler, useEffect, useState } from "react";
 import { getInitials } from "@/lib/utils";
@@ -49,14 +48,12 @@ import { Toaster } from "@/components/ui/toaster";
 import grupo from "@/public/adm/group.png"
 import integra from "@/public/adm/integra.png"
 
-
 const barlow = Barlow({
   subsets: ["latin"],
   weight: "400",
   variable: "--font-sans",
   display: "swap"
 });
-
 
 export default function RootLayout({
   children,
@@ -69,13 +66,33 @@ export default function RootLayout({
   const [email, setEmail] = useState<string>('')
   const [adminToggle, setAdminToggle] = useState<boolean>(false)
   const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false)
   const { toast } = useToast()
   const { push } = useRouter()
+  
 
   useEffect(() => {
     setUserName(getLocalStorage('name') ?? '')
     setProfilePic(getLocalStorage('picture') ?? '')
     setEmail(getLocalStorage('email') ?? '')
+    
+
+    // Verificar autenticação
+    const checkAuth = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/auth/check-auth', {
+          method: 'GET',
+          credentials: 'include', // Importante para enviar cookies de sessão
+        });
+        const data = await response.json();
+        setIsAuthenticated(data);
+      } catch (error) {
+        console.error('Erro ao verificar autenticação:', error);
+        setIsAuthenticated(false);
+      }
+    };
+
+    checkAuth();
   }, [])
 
   function handleClick(admin: boolean) {
@@ -100,7 +117,8 @@ export default function RootLayout({
             <Image width="40" height="50" src={grupo} alt="Buzios" />
             <Image width="107" height="30" src={integra} alt="Integra" />
           </Link>
-            
+        
+          {isAuthenticated && (
           <Menubar className="rounded-full h-12 text-primary900 
             w-[310px] flex justify-between font-bold">
             <MenubarMenu>
@@ -120,79 +138,91 @@ export default function RootLayout({
               </Button>
             </MenubarMenu>
           </Menubar>
+          )}
 
             <div className="flex items-center gap-10 text-surface">
+            
+            {!isAuthenticated && (
+              <>
+              <Button
+                className="bg-transparent text-white hover:bg-[#FF9F1C] rounded-full px-6 py-2 transition-colors duration-300"
+              >
+                Entrar na conta
+              </Button>
+              <Button
+                className="bg-[#FF9F1C] text-white hover:!bg-transparent rounded-full px-6 py-2 transition-colors duration-300"
+              >
+                Crie sua conta
+              </Button>
+            </>
+            )}
+
               <span className="ml-auto" />
-              <Sheet>
-                <SheetTrigger className="rounded-full bg-transparent flex 
-              flex-row items-center p-1">
-                  <Avatar className="w-8 h-8 mr-3 rounded-full bg-surface">
-                    <AvatarImage src={profilePic} className="rounded-full" />
-                    <AvatarFallback className="text-onSurface">{"CN" ||getInitials(userName)}</AvatarFallback>
-                  </Avatar>
-                  <span className="mr-2"><b>{"Admin Silva" || userName.split(' ')[0]}</b></span>
-                  <ChevronDown size={16} strokeWidth={2.5} />
-                </SheetTrigger>
-                <SheetContent className="py-6 px-6 w-[310px]">
-                  <SheetHeader>
-                    <SheetTitle className="flex flex-row items-center gap-4">
-                      <Avatar className="w-12 h-12 mr-3">
-                        <AvatarImage src={profilePic} className="rounded-full" />
-                        <AvatarFallback>{getInitials(userName)}</AvatarFallback>
-                      </Avatar>
-                      <div className="flex flex-col">
-                        <p className="font-bold">{userName}</p>
-                        <p className="text-sm">{email}</p>
-                      </div>
-                      {/* <ChevronUp size={16} strokeWidth={2.5} /> */}
-                    </SheetTitle>
-                  </SheetHeader>
-                  <div className="flex flex-col mt-8 gap-2 font-medium">
-                    {/* <Link className="rounded-lg p-3 inline-flex gap-2 hover:bg-orange 
-                  hover:text-white" href="/upload">
-                  <BookmarkPlus strokeWidth={1.5} />
-                  Adicionar artigo
-                </Link> */}
-                    <Button className="rounded-lg p-3 py-6 inline-flex gap-2 hover:bg-orange 
-                    hover:text-white text-black text-md bg-transparent justify-start"
-                    >
-                      <RefreshCcw strokeWidth={1.5} />
-                      opcao 1
-                    </Button>
-                    <Dialog open={isOpen} onOpenChange={setIsOpen}>
-                      <DialogTrigger className="rounded-lg p-3 inline-flex gap-2 hover:bg-orange 
+              {isAuthenticated && (
+                <Sheet> 
+                  <SheetTrigger className="rounded-full bg-transparent flex 
+                flex-row items-center p-1">
+                    <Avatar className="w-8 h-8 mr-3 rounded-full bg-surface">
+                      <AvatarImage src={profilePic} className="rounded-full" />
+                      <AvatarFallback className="text-onSurface">{"CN" ||getInitials(userName)}</AvatarFallback>
+                    </Avatar>
+                    <span className="mr-2"><b>{"Admin Silva" || userName.split(' ')[0]}</b></span>
+                    <ChevronDown size={16} strokeWidth={2.5} />
+                  </SheetTrigger>
+                  <SheetContent className="py-6 px-6 w-[310px]">
+                    <SheetHeader>
+                      <SheetTitle className="flex flex-row items-center gap-4">
+                        <Avatar className="w-12 h-12 mr-3">
+                          <AvatarImage src={profilePic} className="rounded-full" />
+                          <AvatarFallback>{getInitials(userName)}</AvatarFallback>
+                        </Avatar>
+                        <div className="flex flex-col">
+                          <p className="font-bold">{userName}</p>
+                          <p className="text-sm">{email}</p>
+                        </div>
+                      </SheetTitle>
+                    </SheetHeader>
+                    <div className="flex flex-col mt-8 gap-2 font-medium">
+                      <Button className="rounded-lg p-3 py-6 inline-flex gap-2 hover:bg-orange 
+                      hover:text-white text-black text-md bg-transparent justify-start"
+                      >
+                        <RefreshCcw strokeWidth={1.5} />
+                        opcao 1
+                      </Button>
+                      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+                        <DialogTrigger className="rounded-lg p-3 inline-flex gap-2 hover:bg-orange 
+                        hover:text-white text-black text-md bg-transparent justify-start">
+                          <User strokeWidth={1.5} />
+                          opcao 2
+                        </DialogTrigger>
+                        <DialogContent>
+                          <DialogHeader>
+                            <DialogTitle>Atualize seu ID Lattes</DialogTitle>
+                            <DialogDescription>Por favor, insira apenas os números do seu ID</DialogDescription>
+                          </DialogHeader>
+
+                          <Input placeholder="Ex: 1234567890"
+                            className="w-full rounded-xl bg-lightGrey border-transparent" />
+                          <DialogFooter className="mt-10 md:justify-between sm:justify-start">
+                            <Button className="bg-orange hover:bg-darkOrange">Enviar</Button>
+                          </DialogFooter>
+
+                        </DialogContent>
+                      </Dialog>
+                      <Button className="rounded-lg p-3 py-6 inline-flex gap-2 hover:bg-orange 
                       hover:text-white text-black text-md bg-transparent justify-start">
-                        <User strokeWidth={1.5} />
-                        opcao 2
-                      </DialogTrigger>
-                      <DialogContent>
-                        <DialogHeader>
-                          <DialogTitle>Atualize seu ID Lattes</DialogTitle>
-                          <DialogDescription>Por favor, insira apenas os números do seu ID</DialogDescription>
-                        </DialogHeader>
+                        <LogOut strokeWidth={1.5} />
+                        Sair
+                      </Button>
 
-                        <Input placeholder="Ex: 1234567890"
-                          className="w-full rounded-xl bg-lightGrey border-transparent" />
-                        <DialogFooter className="mt-10 md:justify-between sm:justify-start">
-                          <Button className="bg-orange hover:bg-darkOrange">Enviar</Button>
-                        </DialogFooter>
-
-                      </DialogContent>
-                    </Dialog>
-                    <Button className="rounded-lg p-3 py-6 inline-flex gap-2 hover:bg-orange 
-                    hover:text-white text-black text-md bg-transparent justify-start">
-                      <LogOut strokeWidth={1.5} />
-                      Sair
-                    </Button>
-
-                  </div>
-                </SheetContent>
-              </Sheet>
+                    </div>
+                  </SheetContent>
+                </Sheet>
+              )}
             </div>
         </header>}
 
         <main className="h-full">{children}</main>
-
 
         <Toaster />
         <footer className="static flex flex-row justify-center 
